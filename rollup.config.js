0fa0fe3fce2a9db0a916cb93a40ba6266b202658
Denis Arh <denis.arh@gmail.com>
2020-02-20 15:22:15 +0100

    non working poc

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+
+export default {
+  input: 'src/server.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+
+    resolve({
+      main: true,
+      preferBuiltins: true,
+    }),
+
+    commonjs(),
+  ],
+}
